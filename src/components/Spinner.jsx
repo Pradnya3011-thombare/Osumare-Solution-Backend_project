79d7564bbda7335fedce010ad3de00dd455dd056
Pradnya3011-thombare <pradnyathombare356@gmail.com>
2024-06-16 19:19:29 +0530

    Add files via upload

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Spinner1 from '../Spinner.gif'
+
+const Spinner = () => {
+  return (
+    <div className='d-flex justify-content-center align-items-center '>
+       <img src={Spinner1} alt="" />
+    </div>
+  ) 
+}
+
+export default Spinner
